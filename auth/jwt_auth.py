5796bd8af1828e2ba88a120aec1e8b928dc24de5
James Titus <titusjames299@gmail.com>
2024-08-23 13:39:05 +0000

    updated jwt

--- auth/jwt_auth.py
+++ auth/jwt_auth.py
@@ -17,12 +17,20 @@ def token_response(token: str):
         "access_token": token
     }
 
-
 def sign_jwt(user_id: str) -> Dict[str, str]:
     payload = {
         "user_id": user_id,
-        "expires": time.time() + 600
+        "expires": time.time() + 2 * 3600
     }
     token = jwt.encode(payload, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
 
     return token_response(token)
+
+
+def decode_jwt(token: str) -> dict:
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
+        return decoded_token if decoded_token["expires"] >= time.time() else None
+    except:
+        return {}
+
