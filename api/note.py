561a9a49c7d99f878f4500198e32033bb8308fd0
jamestitus299 <titusjames299@gmail.com>
2024-08-24 16:37:12 +0530

    updated code -- join

--- api/note.py
+++ api/note.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from fastapi import APIRouter, Depends, HTTPException, Response, status
 from auth.auth_bearer import JWTBearer
 from db_conn.db import get_db_connection, close_connection
@@ -14,7 +15,11 @@ async def create_note(note: Note, response: Response):
         client = get_db_connection()
         db = client["rb_database"]
         users = db.notes
-        result = users.insert_one(note.model_dump())
+        note_value = {
+            "user" : ObjectId(note.user),
+            "note" : note.note
+        }
+        result = users.insert_one(note_value)
         inserted_id = str(result.inserted_id)
 
         if(result.acknowledged):
@@ -53,5 +58,61 @@ async def get_user_note(userId: UserId, response: Response):
         # print(e)
         response.status_code = 500
         return {'error': "Could not retrieve Notes"}, status.HTTP_500_INTERNAL_SERVER_ERROR
+    finally:
+        close_connection(client)
+
+
+# /all -- Endpoint to retrieve all notes, with the user who create it (join) (intended for use by admin )
+@router.post('/all', status_code=200, description="Retrieve all notes (only for admin user)", tags=["note"], dependencies=[Depends(JWTBearer())])
+async def get_all_user_task(userId: UserId, response: Response):
+    try:
+        # print(userId.userId)
+        userId = str(userId.userId)
+        client = get_db_connection()
+        db = client["rb_database"]
+        users = db.users
+
+        getUser = users.find_one({"_id": ObjectId(userId)})
+        if getUser and getUser["userType"] != 1:
+            response.status_code = 403
+            return {"error": "Access Forbidden"}, status.HTTP_403_FORBIDDEN
+
+        notes = db.notes
+        # Perform the join using $lookup (join on notes and users)
+        pipeline = [
+            {
+                "$lookup": {
+                    "from": "users",
+                    "localField": "user", 
+                    "foreignField": "_id",  
+                    "as": "user_info"  
+                }
+            },
+            {
+                "$project": {
+                    "note": 1,
+                    "user_info.name": 1,
+                    "user_info.email": 1,
+                    "_id": 0
+                }
+            }
+        ]
+
+        # Execute the aggregation pipeline
+        results = notes.aggregate(pipeline)
+        # for document in results:
+        #     print(document)
+
+        note_list = [note for note in results]    
+        if len(note_list) == 0:
+            response.status_code = 404
+            return {'msg': "No Notes"}, status.HTTP_404_NOT_FOUND
+        
+        # print(note_list)
+        return {'notes': note_list }, status.HTTP_200_OK
+    except Exception as e:
+        print(e)
+        response.status_code = 500
+        return {'error': "Could not retrieve Tasks"}, status.HTTP_500_INTERNAL_SERVER_ERROR
     finally:
         close_connection(client)
--- api/task.py
+++ api/task.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from fastapi import APIRouter, Depends, HTTPException, Response, status
 from auth.auth_bearer import JWTBearer
 from db_conn.db import get_db_connection, close_connection
@@ -14,7 +15,11 @@ async def create_task(task: Task, response: Response):
         client = get_db_connection()
         db = client["rb_database"]
         tasks = db.tasks
-        result = tasks.insert_one(task.model_dump())
+        task_value = {
+            "user" : ObjectId(task.user),
+            "task" : task.task
+        }
+        result = tasks.insert_one(task_value)
         inserted_id = str(result.inserted_id)
 
         if(result.acknowledged):
@@ -39,8 +44,7 @@ async def get_user_task(userId: UserId, response: Response):
         client = get_db_connection()
         db = client["rb_database"]
         tasks = db.tasks
-        userId = str(userId.userId)
-        results = tasks.find({"user": userId}, {"task": 1, "_id": 0 }).limit(10)
+        results = tasks.find({"user": ObjectId(userId.userId)}, {"task": 1, "_id": 0 }).limit(10)
         
         task_list = [task["task"] for task in results]    
         if len(task_list) == 0:
@@ -57,27 +61,57 @@ async def get_user_task(userId: UserId, response: Response):
         close_connection(client)
     
 
-# /user -- Endpoint to retrieve all tasks, with the user who create it (join) (intended for use by admin )
+# /all -- Endpoint to retrieve all tasks, with the user who create it (join) (intended for use by admin )
 @router.post('/all', status_code=200, description="Retrieve all tasks (only for admin user)", tags=["task"], dependencies=[Depends(JWTBearer())])
 async def get_all_user_task(userId: UserId, response: Response):
     try:
         # print(userId.userId)
+        userId = str(userId.userId)
         client = get_db_connection()
         db = client["rb_database"]
+        users = db.users
+
+        getUser = users.find_one({"_id": ObjectId(userId)})
+        if getUser and getUser["userType"] != 1:
+            response.status_code = 403
+            return {"error": "Access Forbidden"}, status.HTTP_403_FORBIDDEN
+
         tasks = db.tasks
-        userId = str(userId.userId)
-        results = tasks.find({"user": userId}, {"task": 1, "_id": 0 }).limit(10)
-        
-        task_list = [task["task"] for task in results]    
+        # Perform the join using $lookup (join on tasks and users)
+        pipeline = [
+            {
+                "$lookup": {
+                    "from": "users",  # Local field to join on (user reference in tasks)
+                    "localField": "user",  # Foreign field in tasks collection
+                    "foreignField": "_id",  # Field in users collection to join on
+                    "as": "user_info"  # Alias for the joined data
+                }
+            },
+            {
+                "$project": {
+                    "task": 1,
+                    "user_info.name": 1,
+                    "user_info.email": 1,
+                    "_id": 0
+                }
+            }
+        ]
+
+        # Execute the aggregation pipeline
+        results = tasks.aggregate(pipeline)
+        # for document in results:
+            # print(document)
+
+        task_list = [task for task in results]    
         if len(task_list) == 0:
             response.status_code = 404
-            return {'error': "User has no Tasks"}, status.HTTP_404_NOT_FOUND
+            return {'error': "No Tasks"}, status.HTTP_404_NOT_FOUND
         
         # print(task_list)
         return {'tasks': task_list }, status.HTTP_200_OK
     except Exception as e:
-        # print(e)
+        print(e)
         response.status_code = 500
-        return {'error': "Could not retrieve Task"}, status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {'error': "Could not retrieve Tasks"}, status.HTTP_500_INTERNAL_SERVER_ERROR
     finally:
         close_connection(client)
