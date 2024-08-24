9d492faf984d81d1d8e2b66a68f3952ce80d5d21
jamestitus299 <titusjames299@gmail.com>
2024-08-24 21:10:29 +0530

    updated  -- connection pool

--- db_conn/db.py
+++ db_conn/db.py
@@ -1,21 +1,21 @@
 import os
 from pymongo import MongoClient
+import queue
 
-# import queue
-#Connection pool 
-# pool_number = 5
-# connection_pool = queue.Queue(maxsize=pool_number)
-# pool_created = False
+# Connection pool 
+pool_number = 10
+connection_pool = queue.Queue(maxsize=pool_number)
+pool_created = False
 
-# def create_pool():
-#     global pool_created
-#     if pool_created:
-#         return
-#     else:
-#         for _ in range(pool_number):
-#             client = MongoClient(os.environ.get('MONGO_CONN_STRING'))
-#             connection_pool.put(client)
-#         pool_created = True
+def create_pool():
+    global pool_created
+    if pool_created:
+        return
+    else:
+        for _ in range(pool_number):
+            client = MongoClient(os.environ.get('MONGO_CONN_STRING'))
+            connection_pool.put(client)
+        pool_created = True
 
 
 # creates a MongoClient and return a connection client
@@ -23,9 +23,8 @@ def get_db_connection():
     """
         gets a connection to the MongoDB database
     """
-    # create_pool()
-    # client = connection_pool.get() # gets a client connection from the connection pool
-    client = MongoClient(os.environ.get('MONGO_CONN_STRING'))
+    create_pool()
+    client = connection_pool.get() # gets a client connection from the connection pool
     return client
 
 # closes the Client connection
@@ -33,5 +32,4 @@ def close_connection(client):
     """
         closes the connection to the MongoDB database
     """
-    # connection_pool.put(client) # mimics connections close (adds the connections back to the connectino pool)
-    client.close()
+    connection_pool.put(client) # mimics connections close (adds the connections back to the connectino pool)
